00c894e273c10a2fdfd2ee598951a3b252b081a3
DavidBenitoCampo <davbecam@gmail.com>
2021-02-16 21:09:01 +0100

    Creación inicial del blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { NewComponent } from './components/new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
