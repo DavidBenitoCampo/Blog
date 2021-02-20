862e0d9786d6359a4a0f46701d66a4f81b745096
DavidBenitoCampo <davbecam@gmail.com>
2021-02-20 19:14:39 +0100

    recogida de los datos en el blog

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post, PostService } from 'src/app/services.service';
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  posts: Post[]
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.getAll()
+      .then(listaposts => {
+        this.posts = listaposts;
+      })
   }
 
 }
